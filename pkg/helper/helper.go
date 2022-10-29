0e694046ac5126f5a7a3304e3f9b2052e85d90d7
Abdurrahman Aydın <abdurrahman262@hotmail.com>
2022-10-29 21:09:52 +0300

    feat: Added helper funcs

---
+++ pkg/helper/helper.go
@@ -0,0 +1,29 @@
+package helper
+
+import (
+	"reflect"
+	"unsafe"
+)
+
+func Contains(s []string, str string) bool {
+	for _, v := range s {
+		if v == str {
+			return true
+		}
+	}
+
+	return false
+}
+
+func B2s(b []byte) string {
+	return *(*string)(unsafe.Pointer(&b))
+}
+
+func S2b(s string) (b []byte) {
+	bh := (*reflect.SliceHeader)(unsafe.Pointer(&b))
+	sh := (*reflect.StringHeader)(unsafe.Pointer(&s))
+	bh.Data = sh.Data
+	bh.Cap = sh.Len
+	bh.Len = sh.Len
+	return b
+}
