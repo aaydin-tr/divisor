0fad32e3a6da087204de78c120a640cb77ab177b
Abdurrahman Aydın <abdurrahman262@hotmail.com>
2022-10-29 21:10:02 +0300

    feat: Added circular_list

---
+++ pkg/list/circular_list.go
@@ -0,0 +1,33 @@
+package circular_list
+
+import "github.com/aaydin-tr/balancer/http"
+
+type Node struct {
+	Proxy *http.HTTPClient
+	Next  *Node
+}
+
+type List struct {
+	Len  uint
+	Head *Node
+	Tail *Node
+}
+
+func NewLinkedList() *List {
+	return &List{}
+}
+
+func (l *List) AddToTail(value *http.HTTPClient) {
+	newNode := &Node{Proxy: value}
+
+	if l.Len == 0 {
+		l.Head = newNode
+		l.Tail = newNode
+	} else {
+		oldTail := l.Tail
+		oldTail.Next = newNode
+		l.Tail = newNode
+	}
+	l.Tail.Next = l.Head
+	l.Len++
+}
