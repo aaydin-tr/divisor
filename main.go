045806bbae16a9db6d873598c597c7dc540f8c47
Abdurrahman Aydın <abdurrahman262@hotmail.com>
2022-11-26 23:41:52 +0300

    feat: sync

--- main.go
+++ main.go
@@ -1,20 +1,36 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"time"
 
 	balancer "github.com/aaydin-tr/balancer/core"
 	"github.com/aaydin-tr/balancer/pkg/config"
+	"github.com/aaydin-tr/balancer/pkg/http"
+
 	"github.com/valyala/fasthttp"
 )
 
 func main() {
-	configFile := config.ParseConfigFile("./config.yaml")
-	config := config.PrepareConfig(configFile)
+	config := config.ParseConfigFile("./config.yaml")
+	config.PrepareConfig()
 
-	proxies := balancer.NewBalancer(config)
+	proxies := balancer.NewBalancer(config, http.NewHttpClient().DefaultHealtChecker, 5*time.Second)
 
-	if err := fasthttp.ListenAndServe(":8000", proxies.Serve()); err != nil {
+	if proxies == nil {
+		fmt.Println("No avaible serves")
+		return
+	}
+
+	server := fasthttp.Server{
+		Handler:               proxies.Serve(),
+		MaxIdleWorkerDuration: 5 * time.Minute,
+		TCPKeepalivePeriod:    5 * time.Minute,
+		TCPKeepalive:          true,
+	}
+
+	if err := server.ListenAndServe(":8000"); err != nil {
 		log.Fatalf("error in fasthttp server: %s", err)
 	}
 }
