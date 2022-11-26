4fe6c8f88357ab39ab41df40f04c00ef2893b88e
Abdurrahman Aydın <abdurrahman262@hotmail.com>
2022-11-26 23:37:31 +0300

    feat: Add healtChecker

--- core/round-robin/round-robin.go
+++ core/round-robin/round-robin.go
@@ -2,26 +2,44 @@ package round_robin
 
 import (
 	"sync/atomic"
+	"time"
 
 	types "github.com/aaydin-tr/balancer/core/types"
 	"github.com/aaydin-tr/balancer/pkg/config"
+	"github.com/aaydin-tr/balancer/pkg/helper"
 	"github.com/aaydin-tr/balancer/proxy"
 	"github.com/valyala/fasthttp"
 )
 
 type RoundRobin struct {
-	servers []*proxy.ProxyClient
-	len     uint64
-	i       uint64
+	servers    []*proxy.ProxyClient
+	serversMap map[string]int
+	len        uint64
+	i          uint64
+
+	healtCheckerFunc types.HealtCheckerType
+	healtCheckerTime time.Duration
 }
 
-func NewRoundRobin(config *config.Config) types.IBalancer {
-	roundRobin := &RoundRobin{}
+func NewRoundRobin(config *config.Config, healtCheckerFunc types.HealtCheckerType, healtCheckerTime time.Duration) types.IBalancer {
+	roundRobin := &RoundRobin{serversMap: make(map[string]int), healtCheckerFunc: healtCheckerFunc, healtCheckerTime: healtCheckerTime}
+
 	for _, b := range config.Backends {
+		if !helper.IsHostAlive(b.GetURL()) {
+			//TODO Log
+			continue
+		}
 		proxy := proxy.NewProxyClient(b)
 		roundRobin.servers = append(roundRobin.servers, proxy)
+		roundRobin.serversMap[b.Addr] = len(roundRobin.servers) - 1
 	}
+
 	roundRobin.len = uint64(len(roundRobin.servers))
+	if roundRobin.len <= 0 {
+		return nil
+	}
+
+	go roundRobin.healtChecker(config.Backends)
 
 	return roundRobin
 }
@@ -35,4 +53,27 @@ func (r *RoundRobin) Serve() func(ctx *fasthttp.RequestCtx) {
 func (r *RoundRobin) next() *proxy.ProxyClient {
 	v := atomic.AddUint64(&r.i, 1)
 	return r.servers[v%r.len]
+}
+
+func (r *RoundRobin) healtChecker(backends []config.Backend) {
+	for {
+		time.Sleep(r.healtCheckerTime)
+		//TODO Log
+		for _, backend := range backends {
+			go func(backend config.Backend) {
+				status := r.healtCheckerFunc(backend.GetURL())
+				index, ok := r.serversMap[backend.Addr]
+				if ok && status != 200 {
+					r.servers = helper.Remove(r.servers, index)
+					r.len = r.len - 1
+					delete(r.serversMap, backend.Addr)
+				} else if !ok && status == 200 {
+					proxy := proxy.NewProxyClient(backend)
+					r.servers = append(r.servers, proxy)
+					r.len++
+					r.serversMap[backend.Addr] = len(r.servers) - 1
+				}
+			}(backend)
+		}
+	}
 }
