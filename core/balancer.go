6e9144e29b9189b01b49604a1d0cf85307030bca
Abdurrahman Aydın <abdurrahman262@hotmail.com>
2022-11-26 23:40:28 +0300

    feat: Add healtCheckerFunc and healtCheckerTime

--- core/balancer.go
+++ core/balancer.go
@@ -1,22 +1,21 @@
 package balancer
 
 import (
-	ip_hash "github.com/aaydin-tr/balancer/core/ip-hash"
-	random "github.com/aaydin-tr/balancer/core/random"
+	"time"
+
 	round_robin "github.com/aaydin-tr/balancer/core/round-robin"
-	w_round_robin "github.com/aaydin-tr/balancer/core/w-round-robin"
 
 	"github.com/aaydin-tr/balancer/core/types"
 	"github.com/aaydin-tr/balancer/pkg/config"
 )
 
-var balancers = map[string]func(config *config.Config) types.IBalancer{
-	"round-robin":   round_robin.NewRoundRobin,
-	"w-round-robin": w_round_robin.NewWRoundRobin,
+var balancers = map[string]func(config *config.Config, healtCheckerFunc types.HealtCheckerType, healtCheckerTime time.Duration) types.IBalancer{
+	"round-robin": round_robin.NewRoundRobin,
+	/*"w-round-robin": w_round_robin.NewWRoundRobin,
 	"ip-hash":       ip_hash.NewIPHash,
-	"random":        random.NewRandom,
+	"random":        random.NewRandom,*/
 }
 
-func NewBalancer(config *config.Config) types.IBalancer {
-	return balancers[config.Type](config)
+func NewBalancer(config *config.Config, healtCheckerFunc types.HealtCheckerType, healtCheckerTime time.Duration) types.IBalancer {
+	return balancers[config.Type](config, healtCheckerFunc, healtCheckerTime)
 }
