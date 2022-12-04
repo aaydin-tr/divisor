e07d11417bb47f1f40286c35664c11a39d7dc936
Abdurrahman Aydın <abdurrahman262@hotmail.com>
2022-12-04 19:52:08 +0300

    fix: healtChecker remove server bug fix

--- core/round-robin/round-robin.go
+++ core/round-robin/round-robin.go
@@ -1,6 +1,7 @@
 package round_robin
 
 import (
+	"sync"
 	"sync/atomic"
 	"time"
 
@@ -13,16 +14,17 @@ import (
 
 type RoundRobin struct {
 	servers    []*proxy.ProxyClient
-	serversMap map[string]int
+	serversMap map[string]*proxy.ProxyClient
 	len        uint64
 	i          uint64
 
 	healtCheckerFunc types.HealtCheckerType
 	healtCheckerTime time.Duration
+	mutex            sync.Mutex
 }
 
 func NewRoundRobin(config *config.Config, healtCheckerFunc types.HealtCheckerType, healtCheckerTime time.Duration) types.IBalancer {
-	roundRobin := &RoundRobin{serversMap: make(map[string]int), healtCheckerFunc: healtCheckerFunc, healtCheckerTime: healtCheckerTime}
+	roundRobin := &RoundRobin{serversMap: make(map[string]*proxy.ProxyClient), healtCheckerFunc: healtCheckerFunc, healtCheckerTime: healtCheckerTime, mutex: sync.Mutex{}}
 
 	for _, b := range config.Backends {
 		if !helper.IsHostAlive(b.GetURL()) {
@@ -31,7 +33,7 @@ func NewRoundRobin(config *config.Config, healtCheckerFunc types.HealtCheckerTyp
 		}
 		proxy := proxy.NewProxyClient(b)
 		roundRobin.servers = append(roundRobin.servers, proxy)
-		roundRobin.serversMap[b.Addr] = len(roundRobin.servers) - 1
+		roundRobin.serversMap[b.Addr] = proxy
 	}
 
 	roundRobin.len = uint64(len(roundRobin.servers))
@@ -61,17 +63,28 @@ func (r *RoundRobin) healtChecker(backends []config.Backend) {
 		//TODO Log
 		for _, backend := range backends {
 			go func(backend config.Backend) {
+				r.mutex.Lock()
+				defer r.mutex.Unlock()
+
 				status := r.healtCheckerFunc(backend.GetURL())
-				index, ok := r.serversMap[backend.Addr]
+				proxy, ok := r.serversMap[backend.Addr]
 				if ok && status != 200 {
+					index, err := helper.FindIndex(r.servers, proxy)
+					if err != nil {
+						//TODO log
+						return
+					}
 					r.servers = helper.Remove(r.servers, index)
 					r.len = r.len - 1
 					delete(r.serversMap, backend.Addr)
+
+					if r.len == 0 {
+						panic("All backends are down")
+					}
 				} else if !ok && status == 200 {
-					proxy := proxy.NewProxyClient(backend)
 					r.servers = append(r.servers, proxy)
 					r.len++
-					r.serversMap[backend.Addr] = len(r.servers) - 1
+					r.serversMap[backend.Addr] = proxy
 				}
 			}(backend)
 		}
