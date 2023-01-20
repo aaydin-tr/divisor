59d863a4ec60e3766da1156e12f89de0f29ef487
memetbulam <mehmet3bulam@gmail.com>
2023-01-20 22:58:06 +0300

    feat: chart dark mode created.

--- internal/monitoring/js/charts/ChartOneOptions.js
+++ internal/monitoring/js/charts/ChartOneOptions.js
@@ -1,5 +1,49 @@
 const ctx = document.getElementById("lineChart");
 
+const darkOption = {
+  scales: {
+    y: {
+      min: 0,
+      max: 100,
+      beginAtZero: true,
+    },
+  },
+
+  elements: {
+    point: {
+      radius: 0,
+      hitRadius: 20,
+    },
+    line: {
+      lineTension: 0.2,
+      backgroundColor: "black",
+    },
+  },
+  animation: true,
+};
+
+const lightOption = {
+  scales: {
+    y: {
+      min: 0,
+      max: 100,
+      beginAtZero: true,
+    },
+  },
+
+  elements: {
+    point: {
+      radius: 0,
+      hitRadius: 20,
+    },
+    line: {
+      lineTension: 0.2,
+      backgroundColor: "red",
+    },
+  },
+  animation: true,
+};
+
 const lineChart = new Chart(ctx, {
   type: "line",
   data: {
@@ -12,42 +56,21 @@ const lineChart = new Chart(ctx, {
       },
     ],
   },
-  options: {
-    scales: {
-      y: {
-        min: 0,
-        max: 100,
-        beginAtZero: true,
-      },
-    },
-
-    elements: {
-      point: {
-        radius: 0,
-        hitRadius: 20,
-      },
-      line: {
-        lineTension: 0.2,
-      },
-    },
-    animation: false,
-  },
+  options: darkOption,
 });
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
-for (let i = 0; i < 50; i++) {
-  setTimeout(() => {
-    const timestamp = dateFormatter(new Date());
-    if (lineChart.data.labels.length > 10) {
-      lineChart.data.labels.shift();
-      lineChart.data.datasets[0].data.shift();
-    }
-    lineChart.data.datasets[0].data.push(getRandomInt(100));
-    lineChart.data.labels.push(timestamp);
+setInterval(() => {
+  const timestamp = dateFormatter(new Date());
+  if (lineChart.data.labels.length > 10) {
+    lineChart.data.labels.shift();
+    lineChart.data.datasets[0].data.shift();
+  }
+  lineChart.data.datasets[0].data.push(getRandomInt(100));
+  lineChart.data.labels.push(timestamp);
 
-    lineChart.update();
-  }, 1000 + i * 500);
-}
+  lineChart.update();
+}, 1000);
