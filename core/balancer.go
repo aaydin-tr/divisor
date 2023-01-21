74cd2212ce1d9a50ae5f0d3201753c815eaad57c
Abdurrahman Aydın <abdurrahman262@hotmail.com>
2023-01-21 22:53:01 +0300

    feat: readd w-round-robin to map

--- core/balancer.go
+++ core/balancer.go
@@ -4,15 +4,15 @@ import (
 	"time"
 
 	round_robin "github.com/aaydin-tr/balancer/core/round-robin"
-
 	"github.com/aaydin-tr/balancer/core/types"
+	w_round_robin "github.com/aaydin-tr/balancer/core/w-round-robin"
 	"github.com/aaydin-tr/balancer/pkg/config"
 )
 
 var balancers = map[string]func(config *config.Config, healtCheckerFunc types.HealtCheckerType, healtCheckerTime time.Duration) types.IBalancer{
-	"round-robin": round_robin.NewRoundRobin,
-	/*"w-round-robin": w_round_robin.NewWRoundRobin,
-	"ip-hash":       ip_hash.NewIPHash,
+	"round-robin":   round_robin.NewRoundRobin,
+	"w-round-robin": w_round_robin.NewWRoundRobin,
+	/*"ip-hash":       ip_hash.NewIPHash,
 	"random":        random.NewRandom,*/
 }
 
